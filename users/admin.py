9d7f94207a49bd8a6a56d825f167f19441e8eaed
orbich67 <orbidan@bk.ru>
2021-10-31 18:13:30 +0300

    added Django admin (Product category, Products, Users, Baskets)

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
 from users.models import User
+from baskets.admin import BasketAdmin
 
-admin.site.register(User)
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'email', 'date_joined')
+    ordering = ('username',)
+    inlines = (BasketAdmin,)

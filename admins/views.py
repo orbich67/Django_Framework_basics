f79ab32035f7ebbe7961b2a974839f01054f5fda
orbich67 <orbidan@bk.ru>
2021-10-31 14:15:49 +0300

    created admins app

---
+++ admins/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {'title': 'Geek Shop - Админ Панель'}
+    return render(request, 'admins/index.html', context)
+
+
+# Create
+def admin_users_create(request):
+    context = {'title': 'Geek Shop - Создание пользователей'}
+    return render(request, 'admins/admin-users-create.html', context)
+
+
+# Read
+def admin_users(request):
+    context = {'title': 'Geek Shop - Пользователи'}
+    return render(request, 'admins/admin-users-read.html', context)
+
+
+# Update
+def admin_users_update(request):
+    context = {'title': 'Geek Shop - Обновление пользователя'}
+    return render(request, 'admins/admin-users-update-delete.html', context)
+
+
+# Delete
+def admin_users_delete(request):
+    pass
